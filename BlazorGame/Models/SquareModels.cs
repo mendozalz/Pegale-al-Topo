a21f8869e053dd3d0ccf9ff6efd5a803bebc9cbb
Lenin Jose Mendoza Espina <mendoza124302@gmail.com>
2024-07-21 22:37:50 -0500

    Agregar archivos de proyecto.

---
+++ BlazorGame/Models/SquareModels.cs
@@ -0,0 +1,23 @@
+﻿namespace BlazorGame.Models
+{
+    public class SquareModels
+    {
+        private bool isShow;
+
+        public int Id { get; set; }
+        public string Styles { get; set; }
+        public bool IsShow { 
+            get { return isShow; } 
+            set { 
+                isShow = value;
+                if (isShow)
+                {
+                    Styles = "topo";
+                }
+                else {
+                    Styles = "";
+                }
+            } 
+        }
+    }
+}
---
+++ BlazorGame/Pages/BlazorGameComponent.cs
@@ -0,0 +1,22 @@
+﻿using BlazorGame.Models;
+
+namespace BlazorGame.Pages
+{
+    public partial class BlazorGameComponent
+    {
+        int score = 0;
+        int life = 10;
+        int hitPosition = 0;
+        string mensaje = "";
+        int velocidadJuego = 1000;
+        bool gameRun = true;
+
+        public List<SquareModels> Squares { get; set; } =  new List<SquareModels>();
+
+        public BlazorGameComponent() {
+            for (int i = 0; i < 9; i++) {
+                Squares.Add(new SquareModels { Id = i });
+            }
+        }
+    }
+}
